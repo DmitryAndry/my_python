1ae496a31052d736069536a1dcafb2b14d2e92ef
DmitryAndry <dimonandrianov@mail.ru>
2020-03-26 15:17:26 +0300

    init commit

---
+++ 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define M 3
+#define N 4
+
+void Ex9 (void)
+{
+    int results[N] = {INT_MAX};
+    int m[M][N] = {{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+
+    for(int i=0, j=0; i<M; i++){
+        for(; j<N; j++) {
+            printf("%5d", m[i][j]);
+            if(results[j] > m[i][j]) results[j] = m[i][j];
+        }
+        puts("");
+    };
+
+    puts("Result:");
+
+    for(int i=0; i<N; i++)
+        printf("%d ", results[i]);
+    puts("");
+};
